e924967f12d18ea61351b9daefe42423ba1a8862
Roberto <43745122+pretended@users.noreply.github.com>
2021-04-03 11:12:39 +0200

    Add files via upload

---
+++ Editor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+<tiptap-vuetify
+      :value="value"
+      :extensions="extensions"
+      @input="$emit('input', arguments[0])"
+    />
+  </div>
+</template>
+<script>
+export default {
+  components: { TiptapVuetify },
+  props: {
+    value: {
+      type: String,
+      default: `<p>Example paragraph</p>`,
+    },
+  },
+  data: () => ({
+    extensions: [
+      History,
+      Blockquote,
+      Link,
+      Underline,
+      Strike,
+      Italic,
+      ListItem,
+      BulletList,
+      OrderedList,
+      [Heading, { options: { levels: [1, 2, 3] } }],
+      Bold,
+      Link,
+      Code,
+      HorizontalRule,
+      Paragraph,
+      HardBreak,
+    ],
+  }),
+};
+</script>
